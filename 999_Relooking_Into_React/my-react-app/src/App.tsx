b7623f1a193fc907b42c71422f1a1257a4fed272
aakku106 <adarashagaihre80@nepathyacollege.edu.np>
2026-02-11 13:36:00 +0545

    removeed testing

--- 999_Relooking_Into_React/my-react-app/src/App.tsx
+++ 999_Relooking_Into_React/my-react-app/src/App.tsx
@@ -1,20 +1,10 @@
-import { useState } from "react"
 import { Two } from "./components/Two"
 
 function App() {
 
-  const [wee, setWee] = useState(["ccn", "cat"])
-  const laa = () => {
-    setWee([
-      "lamooooooo"])
-  }
   return (
     <>
       <Two />
-      {wee}
-      <br />
-      <button onClick={laa} >weeeeeeeeeeeeeeeeeeeeeeeeeeee </button>
-      <br />
     </>
   )
 }
