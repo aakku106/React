17973c66404c7a4eee5977e786f122ce8b14b472
aakku106 <adarashagaihre80@nepathyacollege.edu.np>
2026-03-09 15:11:40 +0545

    persitnt on relosd

--- 999_Relooking_Into_React/my-react-app/src/components/Five.tsx
+++ 999_Relooking_Into_React/my-react-app/src/components/Five.tsx
@@ -2,22 +2,29 @@ import { useEffect, useState } from "react"
 
 export const Five = () => {
   const [data, setData] = useState("")
+  const [holdData, setHoldData] = useState("")
 
   // Get Data
   useEffect(() => {
     const saved = localStorage.getItem("FiveTestData")
+    console.log("dataFetched")
     if (saved) setData(JSON.parse(saved))
   }, [])
-  // Set Data
+
   useEffect(() => {
-    localStorage.setItem("FiveTestData", JSON.stringify(data))
+    if (data) localStorage.setItem("FiveTestData", JSON.stringify(data))
   }, [data])
+
+  const saveData = () => {
+    setData(holdData)
+  }
 
   return (
     <>
       <h1>Data stored: {data}</h1>
       <div>
-        <input type="text" />
+        <input type="text" onChange={(e) => { setHoldData(e.target.value) }} />
+        <button onClick={() => { saveData() }} >SAVE</button>
       </div>
     </>
   )
