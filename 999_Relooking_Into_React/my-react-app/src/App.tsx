e046beda2b65964ce89c77ba5336eec03eb6f715
aakku106 <adarashagaihre80@nepathyacollege.edu.np>
2026-03-09 13:31:12 +0545

    next change beofre

--- 999_Relooking_Into_React/my-react-app/src/App.tsx
+++ 999_Relooking_Into_React/my-react-app/src/App.tsx
@@ -3,25 +3,9 @@ import { Four } from "./components/Four"
 
 const App = () => {
 
-  let [toShow, setTOShow] = useState<boolean>(true);
-  const clearInterval = () => {
-    setTOShow((p) => !p)
-  }
-
   return (
     <>
-      {
-        toShow && <Four />
-      }
 
-      <button onClick={
-        () => clearInterval()
-      } >
-        {
-          toShow ? "StopInterval" : "StartInterval"
-        }
-
-      </button>
     </>
   )
 }
---
+++ 999_Relooking_Into_React/my-react-app/src/components/Five.tsx
@@ -0,0 +1,11 @@
+import { useState } from "react"
+
+export const Five = () => {
+  const [data, setData] = useState('data')
+  return (
+    <>
+
+    </>
+  )
+}
+
